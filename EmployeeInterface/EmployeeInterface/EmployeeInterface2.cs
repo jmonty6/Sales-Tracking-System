fbd9dcedae8edbfe22be1cca38acf5b36af34cbe
Joe Montgomery <jmonty6@gmail.com>
2015-04-28 18:41:41 -0500

    Revert "sdkfjsdf"
    This reverts commit 904fb4b21d5aec71e15a185f14e776b27317eec7.

--- EmployeeInterface/EmployeeInterface/EmployeeInterface2.cs
+++ EmployeeInterface/EmployeeInterface/EmployeeInterface2.cs
@@ -14,8 +14,8 @@ namespace EmployeeInterface
     {
         // Data Members
 
-        private List<Quote> quoteList = new List<Quote>();
-        private List<string> quoteNames = new List<string>();
+        private List<Quote> quoteList;
+        private List<string> quoteNames;
 
         private QuoteSanctioner qs = new QuoteSanctioner();
         private QuoteConverter qc = new QuoteConverter();
@@ -35,10 +35,12 @@ namespace EmployeeInterface
         {
             // clear out the quote selection box
 
-            quoteNames.Clear();
             selectQuoteBox.DataSource = null;
+            selectQuoteBox.Update();
 
             // pass the value in the customer search box to QuoteSanctioner
+
+            quoteNames = new List<string>();
 
             if (customerSearchBox.Text != "")
             {
@@ -64,10 +66,12 @@ namespace EmployeeInterface
         {
             // clear out the quote selection box
 
-            quoteNames.Clear();
             selectQuoteBox.DataSource = null;
+            selectQuoteBox.Update();
 
             // pass the value in the quote search box to QuoteSanctioner
+
+            quoteNames = new List<string>();
 
             if (quoteSearchBox.Text != "")
             {
